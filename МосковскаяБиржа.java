d8eb03fc529db239b0a58ce3b8eb88eccb9dc386
imadham <imad.hamameh@gmail.com>
2019-05-03 12:52:37 +0300

    result

---
+++ МосковскаяБиржа.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package московская.биржа;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author imad
+ */
+public class МосковскаяБиржа {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        string array for storing all
+//        alphabets for all numbering system
+        String[]alphabets = new String[36];
+        //build the alphabet
+        //string[1] = "01",String[2] = "012"....String[36]="012...xyz"
+        for(int i=0;i<36;i++)
+        {
+            int k=0;
+            alphabets[i]="";
+            for(int j=48;j<123;j++)
+            {
+                if(j==58)j=97;
+                alphabets[i]+=(char)j;
+                if(k==i)break;
+                k++;
+            }
+        }
+        
+        Scanner input = new Scanner(System.in);
+        int a,b;
+        String x;
+        int temp=0;
+        a=input.nextInt();
+        b=input.nextInt();
+        input.nextLine();
+        x=input.nextLine();
+
+        try
+        {
+            temp = Integer.parseInt(x,a);
+        }catch(Exception e)
+        {
+            System.err.println("you have entered very big number or number in wrong format"
+                    + "\nplease enter smaller number and in correct format");
+        } 
+        String result=recursiveConvert(temp,alphabets[b-1],b);
+        System.out.println(result);
+    }
+    
+    private static String recursiveConvert(int number,String alphabet,int base) {
+        StringBuilder builder = new StringBuilder();
+        if (number > 0) {
+            String resultNumber = recursiveConvert(number / base,alphabet,base);
+            int digit = number % base;
+            builder.append(resultNumber + alphabet.charAt(digit));
+        }
+        return builder.toString();
+    }
+    
+}
